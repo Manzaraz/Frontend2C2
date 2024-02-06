cc4b39b9c5cc144f23b379fe0fb82ee244b1e3dc
Christian Manzaraz <magnusmanz@gmail.com>
2024-02-05 21:39:42 -0300

    Clase 01: Inicio

---
+++ Semana01/clase2.js
@@ -0,0 +1,32 @@
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 2                                 */
+/* -------------------------------------------------------------------------- */
+// 👇 Esta funcion nos devuelve 1, 2 o 3 según la elección del usuario.
+// Hasta que el usuario ingrese un dato válido le seguimos pidiendo que elija.
+
+function pedirJugada() {
+  
+}
+// let jugadaUsuario = pedirJugada() // Probamos la jugada
+
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 3                                 */
+/* -------------------------------------------------------------------------- */
+function jugadaRandom() {
+    // Math.random()👉🏻 https://www.w3schools.com/js/js_random.asp
+
+}
+
+// let jugadaPC = jugadaRandom() // Probamos la jugada
+
+
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 4                                 */
+/* -------------------------------------------------------------------------- */
+// 👇 Esta funcion nos devuelve el resultado de la partida según las elecciones.
+// Comparamos la eleccion de cada uno para saber quien pierde y quien gana.
+
+function compararJugadas() {
+
+
+// const resultadoDePartida = compararJugadas()
