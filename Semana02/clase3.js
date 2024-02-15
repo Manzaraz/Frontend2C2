c904fde5d24e88e3e0a5f446c6ff2390b227e988
Christian Manzaraz <magnusmanz@gmail.com>
2024-02-15 00:28:08 -0300

    Clase 03 y 04: Fin de clase

--- Semana02/clase3.js
+++ Semana02/clase3.js
@@ -1,30 +1,76 @@
 // 👇Acá vemos que el document nos dá acceso a todo el DOM, ese arbol que contiene todos los nodos de nuestro sitio
-console.log(document)
+// console.log(document)
 
 /*
 Ahora vamos a utilizar 2 métodos propios de document que nos facilitan "pescar" elementos en el sitio:
 - querySelector()
 - querySelectorAll()
 */
+/// Obetener el titulo principal
+const titulo = document.querySelector("h1")
+// console.log(titulo);
+// console.log(titulo.textContent);
 
+const itemsMenu = document.querySelector(".info")
+const infoClima = document.querySelector(".info .clima")
+const navItems = document.querySelectorAll("ul li")
+const image = document.querySelector("[src='./img/futbol.webp']") // busco la etiqueta que tiene el atributo src de la imgen
+
+
+// console.log(itemsMenu);
 
 /* ------------------------------- Practicando ------------------------------ */
 // Seleccionamos la lista de noticias y revisamos su informacion interna.
 // Aprovechamos que la NodeList es un ITERABLE, entonces podemos recorrerla.
 // 🚩 No es un Array.
-
+const articulos = document.querySelectorAll("article")
+console.log(articulos);
 
 // 💪🏼 Ahora llevemos esto un paso adelante!
+// for (let i = 0; i < articulos.length; i++) {
+//     console.log(articulos[i].innerHTML);
+// }
+
+articulos.forEach(articulo => {
+    const titular = articulo.querySelector("h2").textContent
+    console.log(titular);
+});
+
 // Vamos a interactuar con el DOM para agregarle mas estilos a nuestro sitio.
 // 👇 Primero capturemos todos los elementos que vamos a modificar.
+const menuItems = document.querySelectorAll("nav li")
+console.log(menuItems);
+console.log(menuItems[0].style.textTransform = "uppercase");
 
-
+menuItems.forEach(item => {
+    item.style.textTransform = "uppercase"
+    item.style.color = "aqua"
+    item.style.backgroundColor = "rgba(255,255,255, 0.2)"
+    item.style.borderRadius = "50vh"
+    item.style.padding = "10px"
+    item.style.marginTop = "20px"
+});
 
 
 
 /* ---------------------------- Editado los ITEMS --------------------------- */
 // 👇 Acá podemos ver todas las propiedades CSS que podemos modificar con JS
 // console.log(menuItems);
+const sitio = document.querySelector("body")
+console.log(sitio);
+
+console.log(sitio.classList);
+console.log(sitio.classList.add("dark"));// agrego la  clase dark
+console.log(sitio.classList.remove("dark")); // elimino la  clase dark
+console.log(sitio.classList.contains("dark")); // solo evaluo si contiene la clase dark
+console.log(sitio.classList.toggle("dark")); // Toggle me devuelve un booleano dependiendo agrego la clase o la elimino
+console.log(sitio.classList.toggle("dark"));
+console.log(sitio.classList.toggle("dark"));
+console.log(sitio.classList.toggle("dark"));
+
+
+
+
 
 
 // Agregamos manualmente nuevos estilos en el menú
--- Semana02/clase4.js
+++ Semana02/clase4.js
@@ -21,14 +21,14 @@ const listadoNoticias = [{
 // Para eso vamos a valernos de array listadoNoticias que tenemos más arriba
 // ¿Cual es la idea? utilizar la información que nos llega del listado para presentarla en pantalla.
 
-// Primero, mantengamos el HTML vinculado solo con clase5 y clase6 👌
+
 
 /* -------------------------- PRACTICANDO ATRIBUTOS ------------------------- */
 // Completemos correctamente el 'alt' de cada imagen con la frase "miniatura de noticia"
-
-
+const imagenes = document.querySelectorAll(".noticias article img")
+console.log(imagenes);
 // rellenamos el atributo👇
-
+imagenes[0].setAttribute("alt","miniatura ilustrativa de la noticia")
 // Hagamoslo más dinámico y recorramos todas las imagenes👇
 
 
@@ -36,10 +36,46 @@ const listadoNoticias = [{
 // 1- Ahora vamos a ir al HTML y eliminar los 3 Article que se encuentran en el main.
 // 2- Comentamos la parte de este código de "Practicando atributos"
 // 3- Vamos a crear de a uno e insertarlos en el HTML usando un bucle👇
+const main = document.querySelector(".noticias")
+main.innerHTML = ""
 
+// Tecnica de insercion de nodos
+listadoNoticias.forEach(noticia => {
+    // Crear elementos nuevos que no existen el dom
+    const article = document.createElement("article")
+    const h2 = document.createElement("h2")
+    const img = document.createElement("img")
+    const p = document.createElement("p")
 
+    // agregamos los textos y los atributos de cada etiqueta creada
+    h2.innerText = noticia.titulo
+    img.setAttribute("src", noticia.foto)
+    img.setAttribute("alt", noticia.titulo)
+    p.textContent = noticia.epigrafe
 
+    // agrego h2, img p a la etiqueta padre "article"
+    article.appendChild(h2)
+    article.appendChild(img)
+    article.appendChild(p)
 
+    // agrego el article creado al elemento padre que que seria el contendor de la noticia
+    main.appendChild(article)
+})
+
+const ultimaHora = {
+    titulo: "'¡Gracias... totales!', la frase que inmortalizó Gustavo Cerati en el concierto de Soda Stereo",
+    epigrafe: "Hace 25 años, el 20 de septiembre de 1997, Gustavo Cerati inmortalizó la frase “¡Gracias... totales!” en el concierto de despedida de Soda Stereo realizado en Argentina.",
+    foto: "https://peru21.pe/resizer/wpsSRS3uiZnI1zgnPqOGp82pEAg=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/H6SGFZX6ORBTJDIIN4VIOXYDA4.jpg"
+}
+
+// Tecnica de template literals
+main.innerHTML += `
+    <article>
+        <h2>${ultimaHora.titulo}</h2>
+        <img src="${ultimaHora.foto}" alt="El mas grande del rock latino">
+        <p>${ultimaHora.epigrafe}.</p>
+    </article>
+`
 
 /* -------------------------------------------------------------------------- */
 /*                          CONSIGNA MESA DE TRABAJO                          */
